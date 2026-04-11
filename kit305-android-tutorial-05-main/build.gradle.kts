9b78404daf15cfab1ad9829d65e2009b7ea63fbd
mhtruong-00 <mhtruong@utas.edu.au>
2026-04-11 12:20:27 +1000

    testing add new file

--- kit305-android-tutorial-05-main/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.10.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    //ADD THIS LINE, NOT THE OTHER LINES
-    id("com.google.gms.google-services") version "4.3.15" apply false
-}
