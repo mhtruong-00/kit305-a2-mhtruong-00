1bfc801ae61fce4da23bf02c1c6491c731ee9bbc
mhtruong-00 <mhtruong@utas.edu.au>
2026-04-11 16:01:31 +1000

    add ProductApiService Retrofit interface for utasbot.dev/kit305_2026

---
+++ A2-base/app/src/main/java/au/edu/utas/kit305/tutorial05/ProductApiService.kt
@@ -0,0 +1,23 @@
+package au.edu.utas.kit305.tutorial05
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ProductApiService {
+
+    @GET("product")
+    suspend fun getAllProducts(): Response<List<Product>>
+
+    @GET("product")
+    suspend fun getProductsByCategory(
+        @Query("category") category: String
+    ): Response<List<Product>>
+
+    @GET("product/{id}")
+    suspend fun getProductById(
+        @Path("id") id: Int
+    ): Response<Product>
+}
+
