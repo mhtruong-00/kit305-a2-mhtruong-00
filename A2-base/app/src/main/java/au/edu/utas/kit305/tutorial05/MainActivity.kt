67859fe19fab32aab09669b7e2b0d71f9adcd410
mhtruong-00 <mhtruong@utas.edu.au>
2026-04-11 15:14:40 +1000

    switch main screen to show houses list

--- A2-base/app/src/main/java/au/edu/utas/kit305/tutorial05/MainActivity.kt
+++ A2-base/app/src/main/java/au/edu/utas/kit305/tutorial05/MainActivity.kt
@@ -1,7 +1,6 @@
 package au.edu.utas.kit305.tutorial05
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -17,6 +16,7 @@ import com.google.firebase.ktx.Firebase
 const val FIREBASE_TAG = "FirebaseLogging"
 const val MOVIE_INDEX = "Movie_Index"
 val items = mutableListOf<Movie>()
+val houses = mutableListOf<House>()
 
 
 class MainActivity : AppCompatActivity()
@@ -29,8 +29,8 @@ class MainActivity : AppCompatActivity()
         ui = ActivityMainBinding.inflate(layoutInflater)
         setContentView(ui.root)
 
-        ui.lblMovieCount.text = "${items.size} Movies"
-        ui.myList.adapter = MovieAdapter(movies = items)
+        ui.lblMovieCount.text = "${houses.size} Houses"
+        ui.myList.adapter = HouseAdapter(houseList = houses)
 
 //vertical list
         ui.myList.layoutManager = LinearLayoutManager(this)
@@ -39,44 +39,30 @@ class MainActivity : AppCompatActivity()
         val db = Firebase.firestore
         Log.d("FIREBASE", "Firebase connected: ${db.app.name}")
 
-//add some data (comment this out after running the program once and confirming your data is there)
-//        val lotr = Movie(
-//            title = "Lord of the Rings: Fellowship of the Ring",
-//            year = 2001,
-//            duration = 9001F
-//        )
-//        val moviesCollection = db.collection("movies")
-//        moviesCollection
-//            .add(lotr)
-//            .addOnSuccessListener {
-//                Log.d(FIREBASE_TAG, "Document created with id ${it.id}")
-//                lotr.id = it.id
-//            }
-//            .addOnFailureListener {
-//                Log.e(FIREBASE_TAG, "Error writing document", it)
-//            }
-
-//get all movies
-        val moviesCollection = db.collection("movies")
+//get all houses
+        val housesCollection = db.collection("houses")
         ui.lblMovieCount.text = "Loading..."
-        moviesCollection
+        housesCollection
             .get()
             .addOnSuccessListener { result ->
-                items.clear() //this line clears the list, and prevents a bug where items would be duplicated upon rotation of screen
-                Log.d(FIREBASE_TAG, "--- all movies ---")
+                houses.clear() //clear before reload to avoid duplicates after config changes
+                Log.d(FIREBASE_TAG, "--- all houses ---")
                 for (document in result)
                 {
                     Log.d(FIREBASE_TAG, document.toString())
-                    val movie = document.toObject<Movie>()
-                    movie.id = document.id
-                    Log.d(FIREBASE_TAG, movie.toString())
+                    val house = document.toObject<House>()
+                    house.id = document.id
+                    Log.d(FIREBASE_TAG, house.toString())
 
-                    items.add(movie)
+                    houses.add(house)
                 }
-//                (ui.myList.adapter as MovieAdapter).notifyDataSetChanged()
-                //you may choose to fix the warning that notifyDataSetChanged() is not specific enough using:
-                (ui.myList.adapter as MovieAdapter).notifyItemRangeInserted(0, items.size)
-                ui.lblMovieCount.text = "${items.size} Movies"
+                // this is fine for now while the list is simple; we will optimize with specific events later
+                ui.myList.adapter?.notifyDataSetChanged()
+                ui.lblMovieCount.text = "${houses.size} Houses"
+            }
+            .addOnFailureListener {
+                Log.e(FIREBASE_TAG, "Error reading houses", it)
+                ui.lblMovieCount.text = "0 Houses"
             }
     }
 
@@ -84,30 +70,23 @@ class MainActivity : AppCompatActivity()
         super.onResume()
         ui.myList.adapter?.notifyDataSetChanged()
     }
-    inner class MovieHolder(var ui: MyListItemBinding) : RecyclerView.ViewHolder(ui.root) {}
+    inner class HouseHolder(var ui: MyListItemBinding) : RecyclerView.ViewHolder(ui.root) {}
 
-    inner class MovieAdapter(private val movies: MutableList<Movie>) : RecyclerView.Adapter<MovieHolder>()
+    inner class HouseAdapter(private val houseList: MutableList<House>) : RecyclerView.Adapter<HouseHolder>()
     {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainActivity.MovieHolder {
-            val ui = MyListItemBinding.inflate(layoutInflater, parent, false)   //inflate a new row from the my_list_item.xml
-            return MovieHolder(ui)                                                            //wrap it in a ViewHolder
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HouseHolder {
+            val ui = MyListItemBinding.inflate(layoutInflater, parent, false)
+            return HouseHolder(ui)
         }
 
         override fun getItemCount(): Int {
-            return movies.size
+            return houseList.size
         }
 
-        override fun onBindViewHolder(holder: MainActivity.MovieHolder, position: Int) {
-            val movie = movies[position]   //get the data at the requested position
-            holder.ui.txtName.text = movie.title
-            holder.ui.txtYear.text = movie.year.toString()//get the data at the requested position
-
-
-            holder.ui.root.setOnClickListener {
-                val i = Intent(holder.ui.root.context, MovieDetails::class.java)
-                i.putExtra(MOVIE_INDEX, position)
-                startActivity(i)
-            }
+        override fun onBindViewHolder(holder: HouseHolder, position: Int) {
+            val house = houseList[position]
+            holder.ui.txtName.text = house.customerName ?: "Unnamed customer"
+            holder.ui.txtYear.text = house.address ?: "No address"
         }
     }
 }
