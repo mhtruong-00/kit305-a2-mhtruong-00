3e4849b334f29c7bd7c1bf667312e8d0ee36b5b0
mhtruong-00 <mhtruong@utas.edu.au>
2026-04-14 09:54:28 +1000

    store room photo url in room model

--- A2-base/app/src/main/java/au/edu/utas/kit305/tutorial05/Room.kt
+++ A2-base/app/src/main/java/au/edu/utas/kit305/tutorial05/Room.kt
@@ -5,6 +5,7 @@ import com.google.firebase.firestore.Exclude
 class Room(
     @get:Exclude var id: String? = null,
     var houseId: String? = null,
-    var name: String? = null
+    var name: String? = null,
+    var photoUrl: String? = null
 )
 
