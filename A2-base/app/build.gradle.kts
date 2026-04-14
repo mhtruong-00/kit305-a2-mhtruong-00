4017cfe79292fb36d0f4e8a79cae1611a1163e6f
mhtruong-00 <mhtruong@utas.edu.au>
2026-04-14 11:06:09 +1000

    remove storage sdk dependency

--- A2-base/app/build.gradle.kts
+++ A2-base/app/build.gradle.kts
@@ -53,5 +53,4 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.firebase:firebase-firestore:25.0.0")
-    implementation("com.google.firebase:firebase-storage")
 }
